6f6979569a90f7e57c1fa2c76ab8a97cde025216
Ivo van der Wijk <github@in.m3r.nl>
2019-12-22 18:35:51 +0100

    proper term restore; error report

--- cmd/ovim/main.go
+++ cmd/ovim/main.go
@@ -3,12 +3,52 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime/debug"
+	"strings"
 
 	"github.com/gdamore/tcell"
 	"github.com/gdamore/tcell/encoding"
 	"gitlab.com/iivvoo/ovim/ovim"
 )
 
+func recoverFromPanic(cleanup func()) {
+	/*
+			A normal debug.Stack() at this point looks like this:
+			goroutine 23 [running]:
+				runtime/debug.Stack(0xc00012a000, 0x584f40, 0xc0000d0120)
+		       /opt/go1.13.1/src/runtime/debug/stack.go:24 +0x9d
+				main.start.func1.1(0xc0000882a0, 0x5c3de0, 0xc00012a000)
+		       /projects/ovim/cmd/ovim/main.go:65 +0x8d
+				panic(0x584f40, 0xc0000d0120)
+		       /opt/go1.13.1/src/runtime/panic.go:679 +0x1b2
+				gitlab.com/iivvoo/ovim/ovim.(*TermUI).RenderTerm(0xc0000b8700)
+		       /projects/ovim/ovim/term.go:90 +0x487
+				main.start.func1(0xc0000882a0, 0x5c3de0, 0xc00012a000, 0xc00008c720, 0xc0000b8700)
+		       /projects/ovim/cmd/ovim/main.go:110 +0x229
+				created by main.start
+
+			We don't care about the trace up until the panic and the line after it,
+			so we need to do some fancy counting to skip that. Also, it's nice to
+			have the actual error at the bottom
+	*/
+	if r := recover(); r != nil {
+		cleanup()
+		s := strings.Split(string(debug.Stack()), "\n")
+		panicCheck := 0
+		fmt.Println(s[0])
+		for _, entry := range s {
+			if panicCheck == 2 {
+				fmt.Println(entry)
+			} else if strings.HasPrefix(entry, "panic(") {
+				panicCheck = 1
+			} else if panicCheck == 1 {
+				panicCheck = 2
+			}
+		}
+		fmt.Printf("%s\n", r)
+	}
+
+}
 func start() {
 
 	s, e := tcell.NewScreen()
@@ -56,13 +96,10 @@ func start() {
 	 * Using mappings (and more) this is mapped to actions
 	 */
 	go func() {
-		defer func() {
-			if r := recover(); r != nil {
-				close(quit)
-				s.Fini()
-				fmt.Println("Recovered in f", r)
-			}
-		}()
+		defer recoverFromPanic(func() {
+			close(quit)
+			s.Fini()
+		})
 		for {
 			update := false
 			ev := s.PollEvent()
