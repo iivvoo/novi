ab7802c8b1fe65bfc9d5de15354aae98108db4a7
Ivo van der Wijk <github@in.m3r.nl>
2019-12-28 18:17:48 +0100

    refactor cursors

--- ovim/cursor.go
+++ ovim/cursor.go
@@ -28,36 +28,41 @@ type Cursors []*Cursor
 
 func (cs Cursors) Move(b *Buffer, movement CursorDirection) {
 	for _, c := range cs {
-		switch movement {
-		case CursorUp:
-			if c.Line > 0 {
-				c.Line--
-				if c.Pos > len(b.Lines[c.Line]) {
-					c.Pos = len(b.Lines[c.Line])
-				}
-			}
-		case CursorDown:
-			// weirdness because empty last line that we want to position on
-			if c.Line < len(b.Lines)-1 {
-				c.Line++
-				if c.Pos > len(b.Lines[c.Line]) {
-					c.Pos = len(b.Lines[c.Line])
-				}
-			}
-		case CursorLeft:
-			if c.Pos > 0 {
-				c.Pos--
-			}
-		case CursorRight:
-			if c.Pos < len(b.Lines[c.Line]) {
-				c.Pos++
-			}
-		case CursorBegin:
-			c.Pos = 0
-		case CursorEnd:
-			c.Pos = len(b.Lines[c.Line])
-		}
+		c.Move(b, movement)
 	}
+}
+
+func (c *Cursor) Move(b *Buffer, movement CursorDirection) {
+	switch movement {
+	case CursorUp:
+		if c.Line > 0 {
+			c.Line--
+			if c.Pos > len(b.Lines[c.Line]) {
+				c.Pos = len(b.Lines[c.Line])
+			}
+		}
+	case CursorDown:
+		// weirdness because empty last line that we want to position on
+		if c.Line < len(b.Lines)-1 {
+			c.Line++
+			if c.Pos > len(b.Lines[c.Line]) {
+				c.Pos = len(b.Lines[c.Line])
+			}
+		}
+	case CursorLeft:
+		if c.Pos > 0 {
+			c.Pos--
+		}
+	case CursorRight:
+		if c.Pos < len(b.Lines[c.Line]) {
+			c.Pos++
+		}
+	case CursorBegin:
+		c.Pos = 0
+	case CursorEnd:
+		c.Pos = len(b.Lines[c.Line])
+	}
+
 }
 
 // MoveEnd, MoveStart - move all cursors to end/start of line
