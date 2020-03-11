98c85d0d826146cb76c93def6c9679074bddb174
Ivo van der Wijk <github@in.m3r.nl>
2020-03-11 15:41:06 +0100

    very simple, primitive line numbers

--- ui/term/term.go
+++ ui/term/term.go
@@ -3,6 +3,7 @@ package termui
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/gdamore/tcell"
@@ -55,8 +56,9 @@ func NewTermUI(Editor *novi.Editor) *TermUI {
 	s.Show()
 
 	w, h := s.Size()
+	log.Printf("Term size w h: %d %d", w, h)
 	// adjust for statusbars and box, to be fixed XXX
-	tui := &TermUI{s, Editor, 0, 0, w - 1, h - 3, w, h, "", "", MainSource, 0, "", ""}
+	tui := &TermUI{s, Editor, 0, 0, w, h, w, h, "", "", MainSource, 0, "", ""}
 	return tui
 }
 
@@ -140,6 +142,8 @@ func (t *TermUI) SetError(message string) {
 	}()
 }
 func (t *TermUI) drawBottomRow(s string, error bool) {
+	// Use the full available width to draw the row, but make sure
+	// status is truncated if too long
 	x := 0
 
 	style := tcell.StyleDefault
@@ -147,12 +151,20 @@ func (t *TermUI) drawBottomRow(s string, error bool) {
 	if error {
 		style = style.Foreground(tcell.ColorWhite).Background(tcell.ColorRed)
 	}
+
+	rowY := t.EditAreaHeight - 1
+	width := t.EditAreaWidth
+
+	if len(s) > width {
+		s = s[:width]
+	}
+
 	for _, r := range s { // XXX May overflow
-		t.Screen.SetContent(x, t.Height-1, r, nil, style)
+		t.Screen.SetContent(x, rowY, r, nil, style)
 		x++
 	}
 	for x < t.EditAreaWidth {
-		t.Screen.SetContent(x, t.Height-1, ' ', nil, tcell.StyleDefault)
+		t.Screen.SetContent(x, rowY, ' ', nil, tcell.StyleDefault)
 		x++
 	}
 
@@ -170,23 +182,51 @@ func (t *TermUI) DrawInput() {
 	t.Screen.ShowCursor(t.inputPos, t.Height-1)
 }
 
+func (t *TermUI) drawGutter(start, end int, guttersize int) {
+	// drawGutter should decide size, return it
+
+	// we need an upper limit - not all rows my be in use
+	for y := 0; y < t.EditAreaHeight-1; y++ {
+		l := ""
+		lineno := y + start
+		if lineno < end {
+			l = strconv.Itoa(start + y + 1)
+		}
+		for len(l) < guttersize-1 {
+			l = " " + l
+		}
+		for x, r := range l {
+			t.Screen.SetContent(x, y, r, nil, tcell.StyleDefault)
+		}
+	}
+}
+
 /*
  Rendering on the screen always starts at (0,0), but characters taken from
  the editor are from a specific viewport
 */
 func (t *TermUI) Render() {
+	/*
+	 t.EditAreaWidth is the size of the editor, t.EditAreaHeight the height.
+	 Should this be purely the editing size, or also include statusbar, line gutter?
+	 It will be set to terminal H/W if not set, so it's actually the entire area to use
+	*/
+
+	guttersize := 4 // 3 for numbers, 1 space)
+
 	t.Width, t.Height = t.Screen.Size()
+	editWidth, editHeight := t.EditAreaWidth-guttersize, t.EditAreaHeight-1
 
 	primaryCursor := t.Editor.Cursors[0]
-	if primaryCursor.Pos > t.ViewportX+t.EditAreaWidth-1 {
-		t.ViewportX = primaryCursor.Pos - (t.EditAreaWidth - 1)
+	if primaryCursor.Pos > t.ViewportX+editWidth-1 {
+		t.ViewportX = primaryCursor.Pos - (editWidth - 1)
 	}
 	if primaryCursor.Pos < t.ViewportX {
 		t.ViewportX = primaryCursor.Pos
 	}
 
-	if primaryCursor.Line > t.ViewportY+t.EditAreaHeight-1 {
-		t.ViewportY = primaryCursor.Line - (t.EditAreaHeight - 1)
+	if primaryCursor.Line > t.ViewportY+editHeight-1 {
+		t.ViewportY = primaryCursor.Line - (editHeight - 1)
 	}
 	if primaryCursor.Line < t.ViewportY {
 		t.ViewportY = primaryCursor.Line
@@ -197,20 +237,25 @@ func (t *TermUI) Render() {
 	 * to clear any remainders. THe latter is relevant when scrolling, for example
 	 */
 	y := 0
-	for _, line := range t.Editor.Buffer.GetLines(t.ViewportY, t.ViewportY+t.EditAreaHeight) {
-		x := 0
-		for _, rune := range line.GetRunes(t.ViewportX, t.ViewportX+t.EditAreaWidth) {
+	for _, line := range t.Editor.Buffer.GetLines(t.ViewportY, t.ViewportY+editHeight) {
+		x := guttersize
+		for _, rune := range line.GetRunes(t.ViewportX, t.ViewportX+editWidth) {
 			t.Screen.SetContent(x, y, rune, nil, tcell.StyleDefault)
 			x++
 		}
-		for x < t.EditAreaWidth {
+		for x < editWidth {
 			t.Screen.SetContent(x, y, ' ', nil, tcell.StyleDefault)
 			x++
 		}
 		y++
 	}
-	for y < t.EditAreaHeight {
-		for x := 0; x < t.EditAreaWidth; x++ {
+	// We draw the gutter now because y contains the number of lines actually drawn (may be
+	// at EOF), but eventually we'll need to do the math upfront and even let drawGutter calc is
+	// own size
+	t.drawGutter(t.ViewportY, t.ViewportY+y, guttersize)
+
+	for y < editHeight {
+		for x := guttersize; x < editWidth+guttersize; x++ {
 			t.Screen.SetContent(x, y, ' ', nil, tcell.StyleDefault)
 		}
 		y++
@@ -218,7 +263,7 @@ func (t *TermUI) Render() {
 	// To make the cursor blink, show/hide it?
 	for _, cursor := range t.Editor.Cursors {
 		if cursor.Line != -1 {
-			t.Screen.ShowCursor(cursor.Pos-t.ViewportX, cursor.Line-t.ViewportY)
+			t.Screen.ShowCursor(cursor.Pos-t.ViewportX+guttersize, cursor.Line-t.ViewportY)
 		}
 		// else probably show at (0,0)
 	}
